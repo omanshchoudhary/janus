5fcd9388e7a65dae00783a5ccfc160a3e72cb116
Omansh Choudhary <omansh.choudhary@gmail.com>
2026-06-20 04:04:17 +0530

    feat(health): add HTTP path probe

--- crates/janus-health/src/lib.rs
+++ crates/janus-health/src/lib.rs
@@ -1,6 +1,7 @@
 use std::{net::SocketAddr, time::Duration};
 use tokio::net::TcpStream;
 use tokio::time::timeout;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 
 #[derive(Debug,Clone)]
 pub struct HealthCheckConfig {
@@ -36,7 +37,31 @@ async fn tcp_connect_check(addr:SocketAddr, timeout_dur: Duration) -> bool{
     }
 }
 
+async fn http_check(addr: SocketAddr, path: &str, timeout_dur: Duration) -> bool {
+    let result = timeout(timeout_dur, async {
+        
+        // connect
+        let mut stream = TcpStream::connect(addr).await.ok()?;
 
+        // create a request line
+        let request = format!(
+             "GET {path} HTTP/1.1\r\nHost: healthcheck\r\nConnection: close\r\n\r\n"
+        );
+
+        // sending request
+        stream.write_all(request.as_bytes()).await.ok()?;
+
+        let mut reader = BufReader::new(stream);
+        let mut status_line = String::new();
+        reader.read_line(&mut status_line).await.ok()?;
+
+        let code = status_line.split_whitespace().nth(1)?.parse::<u16>().ok()?;
+        Some((200..=299).contains(&code))
+    }) 
+    .await;
+
+    matches!(result, Ok(Some(true)))
+}
 
 
 pub fn janus_health() -> &'static str {
